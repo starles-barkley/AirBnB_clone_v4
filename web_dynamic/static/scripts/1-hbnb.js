28bba0b2c2d8ab93df98712cf419d0f48569b77e
iamseaniam <7222@holbertonstudents.com>
2024-04-05 13:28:01 -0500

    changed style for code to match what ever the name of it is called

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -9,27 +9,23 @@ update the h4 tag inside the div Amenities with the list of Amenities checked
 */
 
 $(document).ready(function() {
-    // Initialize an empty dictionary to store the checked amenity IDs
-    const checkedAmenities = {};
-
-    // Listen for changes on each input checkbox tag
-    $('input[type="checkbox"]').change(function() {
-        // Get the amenity ID from the checkbox's data-id attribute
-        const amenityId = $(this).data('id');
-
-        // Check if the checkbox is checked
-        if ($(this).is(':checked')) {
-            // If checked, add the amenity ID to the dictionary
-            checkedAmenities[amenityId] = true;
-        } else {
-            // If unchecked, remove the amenity ID from the dictionary
-            delete checkedAmenities[amenityId];
-        }
-
-        // Convert the dictionary keys to an array and join them with a comma
-        const amenitiesList = Object.keys(checkedAmenities).join(', ');
-
-        // Update the <h4> tag inside the <div> with the class 'Amenities' with the list of checked amenities
-        $('.Amenities h4').text(amenitiesList);
-    });
+  // Initialize an empty dictionary to store the checked amenity IDs
+  const checkedAmenities = {};
+  // Listen for changes on each input checkbox tag
+  $('input[type="checkbox"]').change(function() {
+    // Get the amenity ID from the checkbox's data-id attribute
+    const amenityId = $(this).data('id');
+    // Check if the checkbox is checked
+    if ($(this).is(':checked')) {
+      // If checked, add the amenity ID to the dictionary
+      checkedAmenities[amenityId] = true;
+    } else {
+      // If unchecked, remove the amenity ID from the dictionary
+      delete checkedAmenities[amenityId];
+    }
+    // Convert the dictionary keys to an array and join them with a comma
+    const amenitiesList = Object.keys(checkedAmenities).join(', ');
+    // Update the <h4> tag inside the <div> with the class 'Amenities' with the list of checked amenities
+    $('.Amenities h4').text(amenitiesList);
+  });
 });
