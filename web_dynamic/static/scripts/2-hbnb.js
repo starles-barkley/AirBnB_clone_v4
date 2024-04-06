66389b1907ae4bdefe1ce25ee3e02673eb69d300
iamseaniam <7222@holbertonstudents.com>
2024-04-06 12:58:46 -0500

    updated 2-hbnb.js to check if the status is okay and adds class, if not it removes class

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,5 +1,12 @@
 #!/usr/bin/node
 /*
+Your script must be executed only when DOM is loaded
+You must use JQuery
+Listen for changes on each input checkbox tag:
+if the checkbox is checked, you must store the Amenity ID in a variable (dictionary or list)
+if the checkbox is unchecked, you must remove the Amenity ID from the variable
+update the h4 tag inside the div Amenities with the list of Amenities checked
+
 Based on 1-hbnb.js
 Request http://0.0.0.0:5001/api/v1/status/:
 If in the status is “OK”, add the class available to the div#api_status
@@ -26,4 +33,16 @@ $(document).ready(function() {
     // Update the <h4> tag inside the <div> with the class 'Amenities' with the list of checked amenities
     $('.Amenities h4').text(amenitiesList);
   });
+
+ // Make an HTTP GET request to the specified URL
+ $.get('http://0.0.0.0:5001/api/v1/status/', function(data) {
+    // Check if the status is "OK"
+    if (data.status === "OK") {
+      // If the status is "OK", add the class "available" to the div#api_status
+      $('#api_status').addClass('available');
+    } else {
+      // Otherwise, remove the class "available" from the div#api_status
+      $('#api_status').removeClass('available');
+    }
+ });
 });
