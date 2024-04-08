84870e67c5d08f12c0f29b99376f3eac4f1ac583
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-04-08 20:18:36 +0000

    add 3-hbnb.py and html and update 2-js

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -7,23 +7,28 @@ Otherwise, remove the class available to the div#api_status
 */
 
 $(document).ready(function() {
-  // Initialize an empty dictionary to store the checked amenity IDs
   const checkedAmenities = {};
-  // Listen for changes on each input checkbox tag
   $('input[type="checkbox"]').change(function() {
-    // Get the amenity ID from the checkbox's data-id attribute
     const amenityId = $(this).data('id');
-    // Check if the checkbox is checked
     if ($(this).is(':checked')) {
-      // If checked, add the amenity ID to the dictionary
       checkedAmenities[amenityId] = true;
     } else {
-      // If unchecked, remove the amenity ID from the dictionary
       delete checkedAmenities[amenityId];
     }
-    // Convert the dictionary keys to an array and join them with a comma
     const amenitiesList = Object.keys(checkedAmenities).join(', ');
-    // Update the <h4> tag inside the <div> with the class 'Amenities' with the list of checked amenities
     $('.Amenities h4').text(amenitiesList);
   });
-});
+  
+  setInterval(function () {
+    $.get('http://0.0.0.0:5001/api/v1/status/') 
+    .done(function(data){
+        $('div#api_status').addClass('available');
+        console.log('api is available');
+      })
+    .fail(function(xhr, status, error) {
+      $('div#api_status').removeClass('available');
+      console.log('api is unavailable');
+    })
+  }, 30000) 
+
+}) 
